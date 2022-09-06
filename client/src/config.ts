ad9241f1540864a2347663878db605b6e92083f1
Dimkpa Chimenim <Dimkpachime@gmail.com>
2022-09-06 09:19:39 +0100

    pushing code from locale

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'e6qmvwe7kk';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-0ygomm8j.us.auth0.com',
+  clientId: '7mICsO56WODmnREHv7hugTyBU215EuDu',
+  callbackUrl: 'http://localhost:3000/callback'
+};
